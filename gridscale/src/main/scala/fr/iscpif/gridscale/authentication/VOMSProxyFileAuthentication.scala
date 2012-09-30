653f6936baa0e03ad019f0f26869fee488546a93
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-09-30 11:45:43 +0200

    Implement VOMS proxy authentication.

--- gridscale/src/main/scala/fr/iscpif/gridscale/authentication/VOMSProxyFileAuthentication.scala
+++ gridscale/src/main/scala/fr/iscpif/gridscale/authentication/VOMSProxyFileAuthentication.scala
@@ -17,6 +17,28 @@
 
 package fr.iscpif.gridscale.authentication
 
-trait ProxyFileVOMSAuthentication extends VOMSAuthentication {
+import java.io.File
+import java.io.FileInputStream
+import org.glite.voms.contact.VOMSProxyInit
+import org.gridforum.jgss.ExtendedGSSManager
+import org.gridforum.jgss.ExtendedGSSCredential
+import org.ietf.jgss.GSSCredential
+
+trait VOMSProxyFileAuthentication {
+  def proxy: File
+  def init = {
+    val proxyBytes = Array.ofDim[Byte](proxy.length.toInt)
+    val in = new FileInputStream(proxy)
+    try in.read(proxyBytes)
+    finally in.close
+
+    ExtendedGSSManager.getInstance.asInstanceOf[ExtendedGSSManager].createCredential(
+      proxyBytes,
+      ExtendedGSSCredential.IMPEXP_OPAQUE,
+      GSSCredential.DEFAULT_LIFETIME,
+      null, // use default mechanism: GSI
+      GSSCredential.INITIATE_AND_ACCEPT)
+  }
 
 }
+
