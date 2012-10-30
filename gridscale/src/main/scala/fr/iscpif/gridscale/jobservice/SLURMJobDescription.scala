a16d40adc5e711935f69f0e4a5a7ee602284ccf7
jH@CKtheRipper <passerat@isima.fr>
2012-10-30 15:09:06 +0100

    Spell my name correctly...

--- gridscale/src/main/scala/fr/iscpif/gridscale/jobservice/SLURMJobDescription.scala
+++ gridscale/src/main/scala/fr/iscpif/gridscale/jobservice/SLURMJobDescription.scala
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 Romain Reuillon
- * Copyright (C) 2012 Jonathan Passerat
+ * Copyright (C) 2012 Jonathan Passerat-Palmbach
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
