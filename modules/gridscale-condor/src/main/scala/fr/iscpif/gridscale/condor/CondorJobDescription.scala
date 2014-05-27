9596f75f192e2d80e7d255426d8ad17c997b09fd
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2014-05-27 18:12:35 +0100

    Start Condor plugin

---
+++ modules/gridscale-condor/src/main/scala/fr/iscpif/gridscale/condor/CondorJobDescription.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2012 Romain
+ * Copyright (C) 2014 Jonathan Passerat-Palmbach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.iscpif.gridscale.condor
+
+import java.util.UUID
+import fr.iscpif.gridscale._
+
+trait CondorJobDescription extends JobDescription {
+  val uniqId = UUID.randomUUID.toString
+  def workDirectory: String
+  // not available yet
+  //  def queue: Option[String] = None
+  //  def wallTime: Option[Int] = None
+  def memory: Option[Int] = None
+  def nodes: Option[Int] = None
+  def coreByNode: Option[Int] = None
+  def output: String = uniqId + ".out"
+  def error: String = uniqId + ".err"
+  def log: String = uniqId + ".log"
+  def requirements: List[String] = List()
+
+  def toCondor = {
+    val buffer = new ScriptBuffer
+    val requirementsBuffer = new ScriptBuffer
+
+    buffer += "output = " + output
+    buffer += "error = " + error
+
+    // TODO: are these features available in Condor?
+    //    queue match {
+    //      case Some(q) ⇒ buffer += "#PBS -q " + q
+    //      case None ⇒
+    //    }
+    //
+    //    wallTime match {
+    //      case Some(t) ⇒
+    //        val df = new java.text.SimpleDateFormat("HH:mm:ss")
+    //        df.setTimeZone(java.util.TimeZone.getTimeZone("GMT"))
+    //        buffer += "#PBS -lwalltime=" + df.format(t * 60 * 1000)
+    //      case None ⇒
+    //    }
+
+    nodes match {
+      case Some(n) ⇒ {
+        buffer += "universe = parallel"
+        buffer += "machine_count = " + n
+        buffer += "request_cpus = " + coreByNode.getOrElse(1)
+      }
+      case None ⇒
+        buffer += "universe = vanilla"
+        coreByNode match {
+          case Some(c) ⇒ requirementsBuffer += "TotalCpus >= " + c
+          case None ⇒
+        }
+    }
+
+    memory match {
+      case Some(m) ⇒ buffer += "request_memory = " + m + " MB"
+      case None ⇒
+    }
+
+    requirements match {
+      case List() ⇒
+      case _ ⇒ requirementsBuffer += requirements.mkString("( ", "&&", " )")
+    }
+
+    buffer += "requirements = " + requirementsBuffer
+    buffer += "initialdir = " + workDirectory
+
+    buffer += "executable = " + executable
+    buffer += "arguments = " + arguments
+    // queue actually submits N jobs (default to 1 in our case)
+    buffer += "queue 1"
+
+    buffer.toString
+  }
+}
---
+++ modules/gridscale-condor/src/main/scala/fr/iscpif/gridscale/condor/CondorJobService.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2012 Romain
+ * Copyright (C) 2014 Jonathan Passerat-Palmbach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.iscpif.gridscale.condor
+
+import fr.iscpif.gridscale._
+import fr.iscpif.gridscale.ssh._
+import SSHJobService._
+
+object CondorJobService {
+  class CondorJob(val description: CondorJobDescription, val condorId: String)
+
+  val jobStateAttribute = "JOB_STATE"
+}
+
+import CondorJobService._
+
+trait CondorJobService extends JobService with SSHHost with SSHStorage {
+  type J = CondorJob
+  type D = CondorJobDescription
+
+  def sourceBashRC = "source ~/.bashrc ; "
+
+  def submit(description: D)(implicit credential: A): J = withConnection { c ⇒
+    withSession(c) { exec(_, "mkdir -p " + description.workDirectory) }
+    val outputStream = openOutputStream(condorScriptPath(description))
+    try outputStream.write(description.toCondor.getBytes)
+    finally outputStream.close
+
+    withSession(c) { session ⇒
+      val command = sourceBashRC + "cd " + description.workDirectory + " && condor_submit " + description.uniqId + ".condor"
+      //      val (ret, output, error) = execReturnCodeOutput(session, command)
+      //      if (ret != 0) throw exception(ret, command, output, error)
+      //
+      //
+      //      if (jobId == null) throw new RuntimeException("condor_submit did not return a JobID")
+      val jobId = "1" // dummy
+      new CondorJob(description, jobId)
+    }
+  }
+
+  def state(job: J)(implicit credential: A): JobState = withConnection(withSession(_) { session ⇒
+    val command = sourceBashRC + "qstat -f " + job.condorId
+
+    val (ret, output, error) = execReturnCodeOutput(session, command)
+
+    ret.toInt match {
+      case 153 ⇒ Done
+      case 0 ⇒
+        val lines = output.split("\n").map(_.trim)
+        val state = lines.filter(_.matches(".*=.*")).map {
+          prop ⇒
+            val splited = prop.split('=')
+            splited(0).trim.toUpperCase -> splited(1).trim
+        }.toMap.getOrElse(jobStateAttribute, throw new RuntimeException("State not found in qstat output: " + output))
+        translateStatus(ret, state)
+      case r ⇒ throw exception(ret, command, output, error)
+    }
+  })
+
+  def cancel(job: J)(implicit credential: A) = withConnection(withSession(_) { exec(_, sourceBashRC + "qdel " + job.condorId) })
+
+  //Purge output error job script
+  def purge(job: J)(implicit credential: A) = withSftpClient { c ⇒
+    rmFileWithClient(condorScriptPath(job.description))(c)
+    rmFileWithClient(job.description.workDirectory + "/" + job.description.output)(c)
+    rmFileWithClient(job.description.workDirectory + "/" + job.description.error)(c)
+    rmFileWithClient(job.description.workDirectory + "/" + job.description.log)(c)
+  }
+
+  def condorScriptPath(description: D) = description.workDirectory + "/" + description.uniqId + ".condor"
+
+  def translateStatus(retCode: Int, status: String) =
+    status match {
+      case "R" ⇒ Running
+      case "I" | "H" ⇒ Submitted
+      case _ ⇒ throw new RuntimeException("Unrecognized state " + status)
+    }
+
+}
