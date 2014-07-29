1652ac2a51c8c9f57755f72890e545acdb3c396f
Romain Reuillon <romain.reuillon@iscpif.fr>
2014-07-29 20:20:21 +0200

    Fix sge workdir attribute.

--- modules/gridscale-sge/src/main/scala/fr/iscpif/gridscale/sge/SGEJobDescription.scala
+++ modules/gridscale-sge/src/main/scala/fr/iscpif/gridscale/sge/SGEJobDescription.scala
@@ -45,7 +45,7 @@ trait SGEJobDescription extends JobDescription {
     queue.foreach(q ⇒ buffer += "#$ -q " + q)
     memory.foreach(m ⇒ buffer += "-l h_vmem=" + m + "M")
     wallTime.foreach(t ⇒ buffer += "-l h_cpu=" + t.toSeconds)
-    buffer += "#$ -wd " + workDirectory
+    buffer += "#$ -cdw " + workDirectory
 
     buffer += executable + " " + arguments
 
