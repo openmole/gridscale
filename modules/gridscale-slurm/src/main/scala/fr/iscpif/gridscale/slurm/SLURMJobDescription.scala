openmole/gridscale

commit 0c3dd422aa856772b050f3428db822a398f51087

Use duration in job descriptions.

modules/gridscale-slurm/src/main/scala/fr/iscpif/gridscale/slurm/SLURMJobDescription.scala
@@ -22,6 +22,8 @@ import java.util.UUID
 import fr.iscpif.gridscale.jobservice.JobDescription
 import fr.iscpif.gridscale.tools.ScriptBuffer
 
+import scala.concurrent.duration.Duration
+
 /** Represent Gres by extending Tuple2 in order to override toString */
 class Gres(val gresName: String, val gresValue: Int) extends Tuple2[String, Int](gresName, gresValue) {
   override def toString = _1 + ":" + _2.toString
@@ -38,7 +40,7 @@ trait SLURMJobDescription extends JobDescription {
   val uniqId = UUID.randomUUID.toString
   def workDirectory: String
   def queue: Option[String] = None
-  def wallTime: Option[Int] = None
+  def wallTime: Option[Duration] = None
   def memory: Option[Int] = None
   def output: String = uniqId + ".out"
   def error: String = uniqId + ".err"
@@ -63,8 +65,11 @@ trait SLURMJobDescription extends JobDescription {
     }
 
     wallTime match {
-      case Some(t) ⇒ buffer += "#SBATCH --time=" + t * 60
-      case None    ⇒
+      case Some(t) ⇒
+        val df = new java.text.SimpleDateFormat("HH:mm:ss")
+        df.setTimeZone(java.util.TimeZone.getTimeZone("GMT"))
+        buffer += "#PBS -lwalltime=" + df.format(t.toMillis)
+      case None ⇒
     }
 
     // must handle empty list separately since it is not done in mkString

modules/gridscale-condor/src/main/scala/fr/iscpif/gridscale/condor/CondorJobDescription.scala
@@ -41,7 +41,7 @@ trait CondorJobDescription extends JobDescription {
   def workDirectory: String
   // not available yet
   //  def queue: Option[String] = None
-  //  def wallTime: Option[Int] = None
+  //  def wallTime: Option[Duration] = None
   def memory: Option[Int] = None
   def nodes: Option[Int] = None
   def coreByNode: Option[Int] = None

modules/gridscale-glite/src/main/scala/fr/iscpif/gridscale/glite/WMSJobDescription.scala
@@ -22,6 +22,8 @@ import fr.iscpif.gridscale._
 import fr.iscpif.gridscale.jobservice.JobDescription
 import fr.iscpif.gridscale.tools.ScriptBuffer
 
+import scala.concurrent.duration.Duration
+
 trait WMSJobDescription extends JobDescription {
   def inputSandbox: Iterable[File]
   def outputSandbox: Iterable[(String, File)]
@@ -31,9 +33,9 @@ trait WMSJobDescription extends JobDescription {
   def stdOutput = ""
   def stdError = ""
   def memory: Option[Int] = None
-  def cpuTime: Option[Int] = None
+  def cpuTime: Option[Duration] = None
   def cpuNumber: Option[Int] = None
-  def wallTime: Option[Int] = None
+  def wallTime: Option[Duration] = None
   def jobType: Option[String] = None
   def smpGranularity: Option[Int] = None
   def retryCount: Option[Int] = None
@@ -45,8 +47,8 @@ trait WMSJobDescription extends JobDescription {
   def requirements =
     "other.GlueCEStateStatus == \"Production\"" +
       memory.map(" && other.GlueHostMainMemoryRAMSize >= " + _).mkString +
-      cpuTime.map(" && other.GlueCEPolicyMaxCPUTime >= " + _).mkString +
-      wallTime.map(" && other.GlueCEPolicyMaxWallClockTime >= " + _).mkString +
+      cpuTime.map(" && other.GlueCEPolicyMaxCPUTime >= " + _.toMinutes).mkString +
+      wallTime.map(" && other.GlueCEPolicyMaxWallClockTime >= " + _.toMinutes).mkString +
       architecture.map(" && other.GlueHostArchitecturePlatformType == \"" + _ + "\"").mkString +
       ce.map(" && (" + _.map("other.GlueCEUniqueID == \"" + _ + "\"").mkString("|") + ")").mkString
 

modules/gridscale-pbs/src/main/scala/fr/iscpif/gridscale/pbs/PBSJobDescription.scala
@@ -21,11 +21,13 @@ import java.util.UUID
 import fr.iscpif.gridscale.jobservice._
 import fr.iscpif.gridscale.tools.ScriptBuffer
 
+import scala.concurrent.duration.Duration
+
 trait PBSJobDescription extends JobDescription {
   val uniqId = UUID.randomUUID.toString
   def workDirectory: String
   def queue: Option[String] = None
-  def wallTime: Option[Int] = None
+  def wallTime: Option[Duration] = None
   def memory: Option[Int] = None
   def nodes: Option[Int] = None
   def coreByNode: Option[Int] = None
@@ -53,7 +55,7 @@ trait PBSJobDescription extends JobDescription {
       case Some(t) ⇒
         val df = new java.text.SimpleDateFormat("HH:mm:ss")
         df.setTimeZone(java.util.TimeZone.getTimeZone("GMT"))
-        buffer += "#PBS -lwalltime=" + df.format(t * 60 * 1000)
+        buffer += "#PBS -lwalltime=" + df.format(t.toMillis)
       case None ⇒
     }
 

commit 5d59c0281c247d8109e3e20f745c74de5be2a008

Fix bug introduced in 0c3dd422aa856772b050f3428db822a398f51087

modules/gridscale-slurm/src/main/scala/fr/iscpif/gridscale/slurm/SLURMJobDescription.scala
@@ -68,7 +68,7 @@ trait SLURMJobDescription extends JobDescription {
       case Some(t) ⇒
         val df = new java.text.SimpleDateFormat("HH:mm:ss")
         df.setTimeZone(java.util.TimeZone.getTimeZone("GMT"))
-        buffer += "#PBS -lwalltime=" + df.format(t.toMillis)
+        buffer += "#SBATCH --time=" + df.format(t.toMinutes)
       case None ⇒
     }
 

project/GridAPIBuild.scala
@@ -44,7 +44,7 @@ trait Settings <: Build {
           <url>https://github.com/romainreuillon/</url>
         </developer>
         <developer>
-          <id>jonathanpasserat</id>
+          <id>jopasserat</id>
           <name>Jonathan Passerat-Palmbach</name>
           <url>https://github.com/jopasserat/</url>
         </developer>