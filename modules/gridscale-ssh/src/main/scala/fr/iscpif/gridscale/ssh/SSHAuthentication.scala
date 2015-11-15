0d4e75718eccd03b6d8eb1a995de48f26bc03dc1
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2015-11-15 14:38:58 +0000

    [SSH] enh: enable compression

--- modules/gridscale-ssh/src/main/scala/fr/iscpif/gridscale/ssh/SSHAuthentication.scala
+++ modules/gridscale-ssh/src/main/scala/fr/iscpif/gridscale/ssh/SSHAuthentication.scala
@@ -32,6 +32,7 @@ trait SSHAuthentication {
     val ssh = new SSHClient(sshDefaultConfig)
     // disable strict host key checking
     ssh.getTransport.addHostKeyVerifier(new PromiscuousVerifier)
+    ssh.useCompression()
     ssh.connect(host, port)
 
     try authenticate(ssh)
